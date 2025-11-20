436c0f9581929394eae666054a99d86778e36cdb
Tyler Anderton <tyleranderton@Traction-MacBook-Pro.local>
2025-11-20 11:41:14 -0600

    enabled nvim VSCode community pack

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,15 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
 ---@type LazySpec
-return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
-}
+if vim.g.vscode then
+  return {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.recipes.vscode" },
+  }
+else
+  return {}
+end
